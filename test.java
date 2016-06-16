6fac728657b8a4e09c093318f6129a9d3f8d700a
joohyung <14wnrkim@gmail.com>
2016-06-16 09:44:03 +0900

    Update test.java

--- test.java
+++ test.java
@@ -2,66 +2,49 @@ private void recursiveGauss(ArrayList<double[]> connection) {
 		//this is made for the parameter for the next recursive method.
 		//it save the result double[] array
 		ArrayList<double[]> recurConnection = new ArrayList<double[]>();
-		
 		//sorting array set 'save_array_whose_root_is_variable' as DESC
 		//to avoid the prime coefficient is '0'
 		Collections.sort(connection, new Comparator<double[]>() {
-
 			@Override
 			public int compare(double[] o1, double[] o2) {
 				// TODO Auto-generated method stub
 				if(o1[0]>o2[0]){return -1;}
-				
 				else if(o1[0]<o2[0]){return 1;}
-				
 				else return 0;
 			}
 		});
-
 		int index = 0;
 		//array1 : pivoting array
 		double[] array1 = connection.get(index);
-		
 		//test
 		System.out.println();
 		for (int i = 0; i < array1.length; i++) {
 			System.out.print(String.format("%.2f ", array1[i]));
 		}
 		System.out.println();
-		//test end
-		
 		//saved to the ArrayList 'backwardGauss' to calculate root at the last time.
 		//the saved array is always 'pivoting array''
 		backwardGauss.add(array1);
-
 		double ratio;
 		while (index < connection.size() - 1) {
-			
 			//array2 : this is rotating array, change every loop
 			double[] array2 = connection.get(index + 1);
-			
 			//temp_result_array : this array will be added to the new ArrayList
 			//						for parameter
 			double[] temp_result_array = new double[array1.length - 1];
-			
 			//ratio is calculated by prime coefficient
 			ratio = array2[0] / array1[0];
-			
 			//calculating part
 			double[] ratioed_array1 = new double[array1.length];
 			for (int i = 0; i < array1.length; i++) {
-				
 				ratioed_array1[i] = ratio * array1[i];
 			}
 			int p1idx = 1;
 			for (int i = 0; i < temp_result_array.length; i++) {
-
 				temp_result_array[i] = array2[p1idx] - ratioed_array1[p1idx];
 				p1idx++;
 			}
-			
 			recurConnection.add(temp_result_array);
-			
 			index++;
 		}
 		
@@ -69,10 +52,8 @@ private void recursiveGauss(ArrayList<double[]> connection) {
 		//if the number of polynomial is more than one than we go one more method.
 		if (recurConnection.size() > 1) {
 			recursiveGauss(recurConnection);
-			
 			//start back substitution
 			backwardCalc();
-			
 			return;
 		} 
 		else {
@@ -83,16 +64,12 @@ private void recursiveGauss(ArrayList<double[]> connection) {
 			//
 			//from here,
 			//we should get two roots, last one, and the second to the last one!
-			
 			/****last one****/
 			double[] lastArr = recurConnection.get(0);
 			double root1 = lastArr[1] / lastArr[0];
-
 			root_array.add(root1);
-			
 			/****second to the last*****/
 			int size = backwardGauss.size();
-			
 			//take the end of the 'backwardGauss'
 			double[] polynomial = backwardGauss.get(size-1);
 			//save the constant
@@ -100,12 +77,9 @@ private void recursiveGauss(ArrayList<double[]> connection) {
 			//calculate
 			constant = constant - root_array.get(0)*polynomial[polynomial.length-2];
 			double root2 = constant / polynomial[polynomial.length-2-1];
-			
 			root_array.add(root2);
-			
 			//the last element (double[]) should be romoved.
 			backwardGauss.remove(size-1);
-			
 			return;
 		}
 	}
@@ -113,14 +87,12 @@ private void recursiveGauss(ArrayList<double[]> connection) {
 	private void backwardCalc() {
 		// TODO Auto-generated method stub
 		int size = backwardGauss.size();
-		
 		//take the end of the 'backwardGauss' 
 		double[] polynomial = backwardGauss.get(size-1);
 		//save the constant
 		double constant = polynomial[polynomial.length-1];
 		//how many times we should loop
 		int loopNum = polynomial.length - 2;
-		
 		int i = 0;
 		while (loopNum > i) {
 			//test
@@ -130,12 +102,9 @@ private void recursiveGauss(ArrayList<double[]> connection) {
 			i++;
 		}
 		double root1 = constant / polynomial[polynomial.length-2-i];
-		
 		root_array.add(root1);
-		
 		//the last element (double[]) should be romoved.
 		backwardGauss.remove(size-1);
-		
 		System.out.println();
 		System.out.println("this is done");
 		System.out.println();
